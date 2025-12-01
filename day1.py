b92e515c4eaa898096fbac613ab3abb8eb18759b
Davide Mario Attebrant Sbrzesny <dmas@kth.se>
2025-12-01 20:44:42 +0100

    Before abandoning self made mod calculations

--- day1.py
+++ day1.py
@@ -1,9 +1,11 @@
+import math
+
 class Counter():
     def __init__(self):
         self.counter = 50
         self.zeroes = 0
     
-    def read_line(self, line):
+    def read_line_old(self, line):
         letter = line[0]
         number = int(line[1:])
 
@@ -11,8 +13,43 @@ class Counter():
             self.counter = (self.counter + number) % 100
         else:
             self.counter = (self.counter - number) % 100
+        
         if self.counter == 0:
             self.zeroes += 1
+    
+    
+    def read_line(self, line):
+        letter = line[0]
+        number = int(line[1:])
+
+        if letter == "R":
+            #self.counter = (self.counter + number) % 100
+            self.modulo_counter(number, True)
+        else:
+            #self.counter = (self.counter - number) % 100
+            self.modulo_counter(number, False)
+        print("counter ", self.counter)
+    
+    def modulo_counter(self, number, is_positive):
+        if is_positive:
+            total = self.counter+number
+            print("total", total)
+            divided = total / 100
+            print("divided", divided)
+            self.zeroes += math.floor(divided)
+            self.counter = (divided - math.floor(divided))*100
+        else:
+            total = self.counter - number
+            divided = (total / 100)*-1
+            #print(divided)
+            if divided >=0 and divided < 1: # We need to catch when we go "down" to zero
+                self.zeroes += 1
+            self.zeroes += math.floor(divided)
+            self.counter = (divided - math.floor(divided))*100
+
+                
+            
+        
     
     def output(self):
         print(self.zeroes)
@@ -27,4 +64,9 @@ def main(file):
             counter.read_line(line.strip())
     counter.output()
 
-main("day1Input.txt")
+#main("day1Input.txt")
+main("test.txt")
+
+
+
+
