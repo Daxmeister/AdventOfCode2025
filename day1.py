0c233f9a965108ed67e0af14842a61f6d29bb20d
Davide Mario Attebrant Sbrzesny <dmas@kth.se>
2025-12-01 21:14:12 +0100

    Solved day 1 task 1 and 2

--- day1.py
+++ day1.py
@@ -5,7 +5,7 @@ class Counter():
         self.counter = 50
         self.zeroes = 0
     
-    def read_line_old(self, line):
+    '''def read_line_old(self, line):
         letter = line[0]
         number = int(line[1:])
 
@@ -15,7 +15,7 @@ class Counter():
             self.counter = (self.counter - number) % 100
         
         if self.counter == 0:
-            self.zeroes += 1
+            self.zeroes += 1'''
     
     
     def read_line(self, line):
@@ -23,49 +23,41 @@ class Counter():
         number = int(line[1:])
 
         if letter == "R":
-            #self.counter = (self.counter + number) % 100
             self.modulo_counter(number, True)
         else:
-            #self.counter = (self.counter - number) % 100
             self.modulo_counter(number, False)
-        print("counter ", self.counter)
     
     def modulo_counter(self, number, is_positive):
+        # We count the number of wrap arounds
+        
         if is_positive:
             total = self.counter+number
-            print("total", total)
-            divided = total / 100
-            print("divided", divided)
-            self.zeroes += math.floor(divided)
-            self.counter = (divided - math.floor(divided))*100
-        else:
-            total = self.counter - number
-            divided = (total / 100)*-1
-            #print(divided)
-            if divided >=0 and divided < 1: # We need to catch when we go "down" to zero
-                self.zeroes += 1
-            self.zeroes += math.floor(divided)
-            self.counter = (divided - math.floor(divided))*100
+            wraps, self.counter = divmod(total, 100)
+            self.zeroes += wraps
 
+        else:
+            if self.counter ==0:
+                self.zeroes -= 1 # In this special case we overcount when we start from 0 and go down
                 
-            
-        
+            total = self.counter-number
+            wraps, self.counter = divmod(total, 100)
+            self.zeroes += -wraps # Because divmod returns "negative wraps"
+            if self.counter ==0: # We need to catch when we go "down" to zero
+                self.zeroes += 1
     
     def output(self):
         print(self.zeroes)
     
 
-
 def main(file):
     counter = Counter()
-
     with open (file) as f:
         for line in f:
             counter.read_line(line.strip())
     counter.output()
 
-#main("day1Input.txt")
-main("test.txt")
+main("day1Input.txt")
+#main("test.txt")
 
 
 
